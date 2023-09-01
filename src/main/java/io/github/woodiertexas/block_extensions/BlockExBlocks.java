edd8a224dbd3ae3aa00bfc58a342fd7b9a6acfd1
woodiertexas <woodietexas@protonmail.com>
2023-08-31 21:25:27 -0700

    update to 1.20.1 + a rewrite

---
+++ src/main/java/io/github/woodiertexas/block_extensions/BlockExBlocks.java
@@ -0,0 +1,57 @@
+package io.github.woodiertexas.block_extensions;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import org.quiltmc.qsl.block.extensions.api.QuiltBlockSettings;
+import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
+
+import java.util.HashMap;
+
+import static io.github.woodiertexas.block_extensions.BlockExtensions.LOGGER;
+import static io.github.woodiertexas.block_extensions.BlockExtensions.MOD_ID;
+
+public class BlockExBlocks {
+	public static HashMap<String, Block> blocksToRegister = new HashMap<>();
+
+	// Blocks
+	public static final Block BUGGED_PISTON = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
+	public static final Block DECO_CHISELED_BOOKSHELF = new Block(QuiltBlockSettings.copyOf(Blocks.OAK_PLANKS));
+	public static final Block DECO_DAYLIGHT_DETECTOR = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
+	public static final Block DECO_JIGSAW = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
+	public static final Block DECO_NETHER_PORTAL = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
+	public static final Block DECO_OBSERVER = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
+	public static final Block DECO_SMITHING_TABLE = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
+	public static final Block DECO_SMITHING_TABLE_TWO = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
+	public static final Block HONEYED_PLANKS = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
+
+
+	public static void init() {
+		blocksToRegister.put("bugged_piston", BUGGED_PISTON);
+		blocksToRegister.put("deco_chiseled_bookshelf", DECO_CHISELED_BOOKSHELF);
+		blocksToRegister.put("deco_daylight_detector", DECO_DAYLIGHT_DETECTOR);
+		blocksToRegister.put("deco_jigsaw", DECO_JIGSAW);
+		blocksToRegister.put("deco_nether_portal", DECO_NETHER_PORTAL);
+		blocksToRegister.put("deco_observer", DECO_OBSERVER);
+		blocksToRegister.put("deco_smithing_table", DECO_SMITHING_TABLE);
+		blocksToRegister.put("deco_smithing_table_two", DECO_SMITHING_TABLE_TWO);
+		blocksToRegister.put("honeyed_planks", HONEYED_PLANKS);
+
+
+		for (String blockId : blocksToRegister.keySet()) {
+			LOGGER.info("Value of blocksToRegister: " + blocksToRegister.get(blockId));
+			registerBlocks(blockId, blocksToRegister.get(blockId));
+		}
+	}
+
+	public static void registerBlocks(String blockId, Block block) {
+		Registry.register(Registries.BLOCK, new Identifier(MOD_ID, blockId), block);
+		Registry.register(Registries.ITEM, new Identifier(MOD_ID, blockId), new BlockItem(block, new QuiltItemSettings()));
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> entries.addItem(block));
+	}
+}
--- src/main/java/io/github/woodiertexas/block_extensions/BlockExtensions.java
+++ src/main/java/io/github/woodiertexas/block_extensions/BlockExtensions.java
@@ -1,64 +1,19 @@
 package io.github.woodiertexas.block_extensions;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.Material;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.sound.BlockSoundGroup;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
-import org.quiltmc.qsl.block.extensions.api.QuiltBlockSettings;
-import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BlockExtensions implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod name as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("Block Extensions");
+
+	public static final String MOD_ID = "block_extensions";
 
 	@Override
 	public void onInitialize(ModContainer mod) {
 		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
-	}
 
-	public static final Block DAYLIGHT_DETECTOR = new Block(QuiltBlockSettings.of(Material.WOOL).strength(1f).sounds(BlockSoundGroup.WOOL));
-	public static final Block BUGGED_PISTON = new Block(QuiltBlockSettings.of(Material.WOOL).strength(1f).sounds(BlockSoundGroup.WOOL));
-	public static final Block DECORATIVE_JIGSAW = new Block(QuiltBlockSettings.of(Material.WOOL).strength(1f).sounds(BlockSoundGroup.WOOL));
-	public static final Block DECORATIVE_OBSERVER = new Block(QuiltBlockSettings.of(Material.WOOL).strength(1f).sounds(BlockSoundGroup.WOOL));
-	public static final Block DECORATIVE_SMITHING_TABLE = new Block(QuiltBlockSettings.of(Material.WOOL).strength(1f).sounds(BlockSoundGroup.WOOL));
-	public static final Block DECORATIVE_SMITHING_TABLE_TWO = new Block(QuiltBlockSettings.of(Material.WOOL).strength(1f).sounds(BlockSoundGroup.WOOL));
-	public static final Block DECORATIVE_NETHER_PORTAL = new Block(QuiltBlockSettings.of(Material.WOOL).strength(1f).sounds(BlockSoundGroup.WOOL));
-	public static final Block HONEYED_PLANKS = new Block(QuiltBlockSettings.of(Material.WOOL).strength(1f).sounds(BlockSoundGroup.WOOL));
-
-
-	static {
-
-		// Block string names
-		String[] blocksArray = {
-				"daylight_detector", "bugged_piston", "decorative_jigsaw", "decorative_observer", "decorative_smithing_table",
-				"decorative_smithing_table_two", "decorative_nether_portal", "honeyed_planks"
-		};
-
-		Block[] anotherBlocksArray = {
-				DAYLIGHT_DETECTOR, BUGGED_PISTON, DECORATIVE_JIGSAW, DECORATIVE_OBSERVER, DECORATIVE_SMITHING_TABLE,
-				DECORATIVE_SMITHING_TABLE_TWO, DECORATIVE_NETHER_PORTAL, HONEYED_PLANKS
-		};
-
-		// Checks to see if the arrays are of the same length. If not, stop the Minecraft process.
-		if (blocksArray.length != anotherBlocksArray.length) {
-			throw new RuntimeException("Mismatched array lengths.\nLength of blocksArray: " + blocksArray.length +
-					"\nLength of anotherBlocksArray: " + anotherBlocksArray.length);
-		}
-
-		// Loops through the length of both arrays and registers the blocks found in said arrays.
-		for (int i = 0; i < blocksArray.length; i++) {
-			Registry.register(Registry.BLOCK, new Identifier("block_extensions", blocksArray[i]), anotherBlocksArray[i]);
-			Registry.register(Registry.ITEM, new Identifier("block_extensions", blocksArray[i]),
-					new BlockItem(anotherBlocksArray[i], new QuiltItemSettings().group(ItemGroup.DECORATIONS)));
-		}
+		BlockExBlocks.init();
 	}
 }
