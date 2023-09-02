f5076e16d12ef0e563ce8c1d83c931ad19e61372
woodiertexas <woodietexas@protonmail.com>
2023-09-02 16:25:40 -0700

    bunch of new blocks + some naming changes

--- src/main/java/gay/debuggy/block_extensions/BlockExBlocks.java
+++ src/main/java/gay/debuggy/block_extensions/BlockExBlocks.java
@@ -4,6 +4,7 @@ import gay.debuggy.block_extensions.blocks.MissingTextureBlock;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.GrassBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
@@ -21,20 +22,23 @@ public class BlockExBlocks {
 	public static HashMap<String, Block> blocksToRegister = new HashMap<>();
 
 	// Blocks
-	public static final Block BUGGED_PISTON = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
+	public static final Block BUGGED_PISTON = new Block(QuiltBlockSettings.copyOf(Blocks.OAK_PLANKS));
+	public static final Block CAST_IRON_BLOCK = new Block(QuiltBlockSettings.copyOf(Blocks.IRON_BLOCK));
 	public static final Block DECO_BARREL = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
 	public static final Block DECO_CHISELED_BOOKSHELF = new Block(QuiltBlockSettings.copyOf(Blocks.OAK_PLANKS));
 	public static final Block DECO_DAYLIGHT_DETECTOR = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
 	public static final Block DECO_JIGSAW = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
-	public static final Block DECO_NETHER_PORTAL = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
 	public static final Block DECO_OBSERVER = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
 	public static final Block DECO_SMITHING_TABLE = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
-	public static final Block DECO_SMITHING_TABLE_TWO = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
 	public static final Block FAKE_COMMAND_BLOCK = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
 	public static final Block FAKE_CHAIN_COMMAND_BLOCK = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
+	public static final Block FAKE_NETHER_PORTAL_BLOCK = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
 	public static final Block FAKE_REPEATING_COMMAND_BLOCK = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
-	public static final Block HONEYED_PLANKS = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
+	public static final Block HONEYED_PLANKS = new Block(QuiltBlockSettings.copyOf(Blocks.OAK_PLANKS));
 	public static final MissingTextureBlock MISSING_TEXTURE = new MissingTextureBlock(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
+	public static final Block STONE_TILE = new Block(QuiltBlockSettings.copyOf(Blocks.STONE));
+	public static final Block THIRTY_SIX = new Block(QuiltBlockSettings.copyOf(Blocks.GRASS_BLOCK));
+	public static final Block WROUGHT_IRON_BLOCK = new Block(QuiltBlockSettings.copyOf(Blocks.IRON_BLOCK));
 
 
 	public static void init() {
@@ -43,19 +47,21 @@ public class BlockExBlocks {
 		blocksToRegister.put("deco_chiseled_bookshelf", DECO_CHISELED_BOOKSHELF);
 		blocksToRegister.put("deco_daylight_detector", DECO_DAYLIGHT_DETECTOR);
 		blocksToRegister.put("deco_jigsaw", DECO_JIGSAW);
-		blocksToRegister.put("deco_nether_portal", DECO_NETHER_PORTAL);
 		blocksToRegister.put("deco_observer", DECO_OBSERVER);
 		blocksToRegister.put("deco_smithing_table", DECO_SMITHING_TABLE);
-		blocksToRegister.put("deco_smithing_table_two", DECO_SMITHING_TABLE_TWO);
+		blocksToRegister.put("cast_iron_block", CAST_IRON_BLOCK);
 		blocksToRegister.put("fake_command_block", FAKE_COMMAND_BLOCK);
 		blocksToRegister.put("fake_chain_command_block", FAKE_CHAIN_COMMAND_BLOCK);
+		blocksToRegister.put("fake_nether_portal_block", FAKE_NETHER_PORTAL_BLOCK);
 		blocksToRegister.put("fake_repeating_command_block", FAKE_REPEATING_COMMAND_BLOCK);
 		blocksToRegister.put("honeyed_planks", HONEYED_PLANKS);
 		blocksToRegister.put("missing_texture", MISSING_TEXTURE);
+		blocksToRegister.put("stone_tile", STONE_TILE);
+		blocksToRegister.put("thirty_six", THIRTY_SIX);
+		blocksToRegister.put("wrought_iron_block", WROUGHT_IRON_BLOCK);
 
 
 		for (String blockId : blocksToRegister.keySet()) {
-			LOGGER.info("Value of blocksToRegister: " + blocksToRegister.get(blockId));
 			registerBlocks(blockId, blocksToRegister.get(blockId));
 		}
 	}
