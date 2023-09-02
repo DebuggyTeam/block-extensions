7437c549786acf93821183d2a815704d94ac95de
woodiertexas <woodietexas@protonmail.com>
2023-09-01 20:26:22 -0700

    fake command blocks and decorative barrel side block

--- src/main/java/io/github/woodiertexas/block_extensions/BlockExBlocks.java
+++ src/main/java/io/github/woodiertexas/block_extensions/BlockExBlocks.java
@@ -21,6 +21,7 @@ public class BlockExBlocks {
 
 	// Blocks
 	public static final Block BUGGED_PISTON = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
+	public static final Block DECO_BARREL = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
 	public static final Block DECO_CHISELED_BOOKSHELF = new Block(QuiltBlockSettings.copyOf(Blocks.OAK_PLANKS));
 	public static final Block DECO_DAYLIGHT_DETECTOR = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
 	public static final Block DECO_JIGSAW = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
@@ -28,12 +29,16 @@ public class BlockExBlocks {
 	public static final Block DECO_OBSERVER = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
 	public static final Block DECO_SMITHING_TABLE = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
 	public static final Block DECO_SMITHING_TABLE_TWO = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
+	public static final Block FAKE_COMMAND_BLOCK = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
+	public static final Block FAKE_CHAIN_COMMAND_BLOCK = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
+	public static final Block FAKE_REPEATING_COMMAND_BLOCK = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
 	public static final Block HONEYED_PLANKS = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
 	public static final Block MISSING_TEXTURE = new Block(QuiltBlockSettings.copyOf(Blocks.WHITE_WOOL));
 
 
 	public static void init() {
 		blocksToRegister.put("bugged_piston", BUGGED_PISTON);
+		blocksToRegister.put("deco_barrel", DECO_BARREL);
 		blocksToRegister.put("deco_chiseled_bookshelf", DECO_CHISELED_BOOKSHELF);
 		blocksToRegister.put("deco_daylight_detector", DECO_DAYLIGHT_DETECTOR);
 		blocksToRegister.put("deco_jigsaw", DECO_JIGSAW);
@@ -41,6 +46,9 @@ public class BlockExBlocks {
 		blocksToRegister.put("deco_observer", DECO_OBSERVER);
 		blocksToRegister.put("deco_smithing_table", DECO_SMITHING_TABLE);
 		blocksToRegister.put("deco_smithing_table_two", DECO_SMITHING_TABLE_TWO);
+		blocksToRegister.put("fake_command_block", FAKE_COMMAND_BLOCK);
+		blocksToRegister.put("fake_chain_command_block", FAKE_CHAIN_COMMAND_BLOCK);
+		blocksToRegister.put("fake_repeating_command_block", FAKE_REPEATING_COMMAND_BLOCK);
 		blocksToRegister.put("honeyed_planks", HONEYED_PLANKS);
 		blocksToRegister.put("missing_texture", MISSING_TEXTURE);
 
