f2edf245a1169850194e9e2a654caff979878b6a
woodiertexas <woodietexas@protonmail.com>
2025-09-21 06:20:19 -0700

    fabric port part 2: use fabric's ModInitializer

--- src/main/java/gay/debuggy/block_extensions/BlockExtensions.java
+++ src/main/java/gay/debuggy/block_extensions/BlockExtensions.java
@@ -1,17 +1,16 @@
 package gay.debuggy.block_extensions;
 
-import org.quiltmc.loader.api.ModContainer;
-import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BlockExtensions implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("Block Extensions");
 
-	public static final String MOD_ID = "block_extensions";
+	public static final String MOD_ID = "block_ex";
 
 	@Override
-	public void onInitialize(ModContainer mod) {
+	public void onInitialize() {
 		BlockExBlocks.init();
 	}
 }
