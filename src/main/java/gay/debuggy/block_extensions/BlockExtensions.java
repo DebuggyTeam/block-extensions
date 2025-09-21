00594b32bc068a01db760450d09f404eca72703e
woodiertexas <woodietexas@protonmail.com>
2025-09-21 06:20:19 -0700

    fabric port part 5: get the mod 99.5% working

--- src/main/java/gay/debuggy/block_extensions/BlockExtensions.java
+++ src/main/java/gay/debuggy/block_extensions/BlockExtensions.java
@@ -1,24 +1,16 @@
 package gay.debuggy.block_extensions;
 
 import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BlockExtensions implements ModInitializer {
-	public static final String MOD_ID = "block_extensions";
+	public static final Logger LOGGER = LoggerFactory.getLogger("Block Extensions");
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final String MOD_ID = "block_ex";
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
 		BlockExBlocks.init();
 		LOGGER.info("pineapple");
 	}
